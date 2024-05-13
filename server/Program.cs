6d285528df9f7bff5481b67fee84a80a75b67b7e
Michele Anderlini <michele.anderlini@gmail.com>
2024-05-13 22:59:59 +0200

    Update Program.cs

--- server/Program.cs
+++ server/Program.cs
@@ -35,14 +35,12 @@ WsServer.Start(ws =>
 
 var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
-
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
-
 // Configure the HTTP request pipeline.
 // if (app.Environment.IsDevelopment())
 // {
@@ -50,11 +48,7 @@ var app = builder.Build();
     app.UseSwaggerUI();
     app.UseDeveloperExceptionPage();
 //}
-
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
