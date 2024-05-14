fbb5c56c0e36b613d2754363dbdc2ed5b171b4a2
Giuseppe7887 <giuseppegravante078@gmail.com>
2024-05-14 10:08:02 +0200

    merged

--- client/main.js
+++ client/main.js
@@ -1,4 +1,17 @@
 
 window.addEventListener("DOMContentLoaded",()=>{
+    const user = localStorage.getItem('user');
+    if(user != '' && user){
+        document.getElementById('login__username').value = user;
+    }
     
+    document.querySelector('#submit').addEventListener('click' , (e) =>{
+        e.preventDefault()
+        let value = document.getElementById('login__username').value;
+        if(value == '') return alert('Username is required !')
+        localStorage.setItem('user', value);
+        // loginInputBackgroundColor: #3b4148; remove
+        //  background-color: var(--loginInputBackgroundColor);
+        // background-color: var();
+    })
 })
