e19a24e8ddbad9645d0ba1a14b5a79f430ec6c36
Franz Amoroso <franz.amoroso@outlook.de>
2024-05-17 00:52:15 +0200

    Upgrade

--- client/quest.js
+++ client/quest.js
@@ -1,4 +1,20 @@
 function qst() {
+
+
+const requestData = {
+  type: "multiple",
+  difficulty: "easy",
+  category: "Entertainment: Music",
+  questionText: "In 2006, which band released their debut album \"A Fever You Can't Sweat Out\"?",
+  correctAnswer: "Panic! At the Disco",
+  incorrectAnswers: [
+    "Twenty One Pilots",
+    "My Chemical Romance",
+    "Fall Out Boy"
+  ],
+  asked: true
+};
+
   const cont = document.createElement("div");
   const contTtlQst = document.createElement("div");
   const progressDiv = document.createElement("div");
@@ -17,18 +33,14 @@ function qst() {
   ttltxt.classList.add("ttltxt");
   contQuestResponse.classList.add("form", "login", "boxQstRs");
 
-  progressBarDiv.style.width = "25%";
-  h2.style.padding = 'var(--boxQst)'
+  progressBarDiv.style.width = "%";
+  h2.style.padding = 'var(--boxQst)';
 
   progressDiv.setAttribute("role", "progressbar");
   progressDiv.setAttribute("aria-label", "Success example");
   progressDiv.setAttribute("aria-valuenow", "25");
   progressDiv.setAttribute("aria-valuemin", "0");
   progressDiv.setAttribute("aria-valuemax", "100");
-
-  h3.innerHTML = "Avatar 0/5";
-  h1.innerHTML = "Domanda 1";
-  h2.innerHTML = "Per cosa si utilizza Git?";
 
   document.body.append(cont);
   cont.append(contTtlQst);
@@ -41,21 +53,47 @@ function qst() {
   contQst.append(h2);
   cont.append(contQuestResponse);
 
+    h3.innerHTML = localStorage.getItem("user") +" " + "0/5";
+    h1.innerHTML = requestData.category;
+    h2.innerHTML = requestData.questionText;
+ 
+ /*  fetch('/web/ws/questions', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(requestData)
+  })
+  .then(response => response.json())
+  .then(data => {
+    // Handle response data
+    console.log('Risposta dal server:', data);
+
+    h3.innerHTML = localStorage.getItem("user") + "0/5";
+    h1.innerHTML = data.category;
+    h2.innerHTML = data.questionText;
+
+  })
+  .catch(error => {
+    console.error('Si è verificato un errore durante la richiesta:', error);
+  }); */
+
   for (let i = 0; i < 4; i++) {
     const contResp = document.createElement("div");
-    const inputResponse = document.createElement("input");
+    const inputResponse = document.createElement("button");
 
     contResp.classList.add("form__filed");
-    inputResponse.classList.add("submit");
+    inputResponse.classList.add("box");
 
-    inputResponse.setAttribute("type", "submit");
+    /* inputResponse.setAttribute("type", "submit"); */
 
-    inputResponse.value = "risposta $";
+    inputResponse.innerText = requestData.incorrectAnswers[i];
 
     contResp.append(inputResponse);
     contQuestResponse.append(contResp);
 }
-
 }
-
 qst()
+
+
+
