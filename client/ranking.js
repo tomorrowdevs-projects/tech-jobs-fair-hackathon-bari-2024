a5bd3b80a01cd3119e1446772986d82d0c237db0
Torepa8 <65725642+Torepa8@users.noreply.github.com>
2024-05-16 12:33:34 +0200

    style table

--- client/ranking.js
+++ client/ranking.js
@@ -1,31 +1,50 @@
-//carichiamo sulla pagina ranking i dati relativi ai punteggi dei giocatori
-//prendendoli da localStorage
 
-//prendiamo i dati dal localStorage
-var players = JSON.parse(localStorage.getItem("players"));
-if (players === null) {
-    players = [];
+let server = "";
+let players = [];
+
+// players = JSON.parse(localStorage.getItem("players"));
+// if (players === null) {
+//     players = [];
+// }
+//prendiamo i dati dal server
+if (server !== "") {
+    fetch(server + "/players")
+        .then(response => response.json())
+        .then(data => {
+            players = data;
+        });
+}
+else {
+    //dati di esempio in caso di variabile server vuoto"
+    players = [{ "id": 0, "name": "Torepa8", "score": 500 }, { "id": 1, "name": "Player1", "score": 400 }];
 }
 
 //creiamo la tabella
 var table = document.createElement("table");
 var tr = document.createElement("tr");
 var th = document.createElement("th");
+tr.style.padding = "10px";
+th.style.padding = "10px";
 th.innerHTML = "Name";
 tr.appendChild(th);
 th = document.createElement("th");
+table.appendChild(tr);
 th.innerHTML = "Score";
 tr.appendChild(th);
-table.appendChild(tr);
+
 for (var i = 0; i < players.length; i++) {
     tr = document.createElement("tr");
     var td = document.createElement("td");
+    td.style.padding = "10px";
     td.innerHTML = players[i].name;
     tr.appendChild(td);
     td = document.createElement("td");
+    td.style.padding = "10px";
     td.innerHTML = players[i].score;
     tr.appendChild(td);
     table.appendChild(tr);
+    tr.style.padding = "10px";
+    th.style.padding = "10px";
 }
 document.getElementById("ranking").appendChild(table);
 
